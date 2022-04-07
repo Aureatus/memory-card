97ba94f3bbaafc3f060bc4e9af98157a1af7f3bd
Zesty Alex <alextknott@gmail.com>
2022-04-07 14:38:07 +0100

    Create shuffleCards function andreturn 4 CardComponents in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React, { useEffect } from "react";
+import CardComponent from "./components/CardComponent";
+
 function App() {
-  return <div className="App"></div>;
+  const shuffleCards = () => {
+    let element = document.querySelector(`.cards`);
+    for (let i = element.children.length; i >= 0; i--) {
+      element.appendChild(element.children[(Math.random() * i) | 0]);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="cards">
+        <CardComponent number={1} shuffleCards={shuffleCards} />
+        <CardComponent number={2} shuffleCards={shuffleCards} />
+        <CardComponent number={3} shuffleCards={shuffleCards} />
+        <CardComponent number={4} shuffleCards={shuffleCards} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
