87ea1e8389c6da61f09f5184c8c8ba53f2446449
Zesty Alex <alextknott@gmail.com>
2022-04-07 15:04:27 +0100

    Add logic for running code when card is clicked more than once

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function CardComponent(props) {
-  return <div onClick={props.shuffleCards}>{props.number}</div>;
+  const [clicked, setClicked] = useState(false);
+
+  const prevClickedRef = useRef();
+
+  const toggleClicked = () => {
+    if (clicked === false) {
+      setClicked(true);
+    } else setClicked(false);
+  };
+
+  useEffect(() => {
+    if (prevClickedRef.current === true && clicked === false) {
+      console.log("Game Over");
+    }
+
+    prevClickedRef.current = clicked;
+  });
+
+  return (
+    <div
+      onClick={() => {
+        props.shuffleCards();
+        toggleClicked();
+      }}
+    >
+      {props.number}
+    </div>
+  );
 }
 export default CardComponent;
