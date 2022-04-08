9c15af2d152d2e792c44b0db045c2c030ceb53fa
Zesty Alex <alextknott@gmail.com>
2022-04-08 16:27:29 +0100

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import CardComponent from "./components/CardComponent";
 import ScoreboardComponent from "./components/ScoreboardComponent";
 
